671038a6960bedd409d76274b522753ea1b71a02
Diego Gutierrez <diego.gutierrez684@gmail.com>
2026-02-06 16:49:43 -0300

    fix bugs

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  /* config options here */
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.pexels.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'grace-services.s3.us-east-1.amazonaws.com', pathname: '/**' },
+      { protocol: "https", hostname: "images.pexels.com", pathname: "/**" },
+      { protocol: "https", hostname: "grace-services.s3.us-east-1.amazonaws.com", pathname: "/**" },
+      { protocol: "https", hostname: "diarioelnortino.cl", pathname: "/**" },
+      { protocol: "https", hostname: "imagenes.elpais.com", pathname: "/**" },
+      { protocol: "https", hostname: "internationalsocialist.net", pathname: "/**" },
+      { protocol: "https", hostname: "cambio21.cl", pathname: "/**" },
+
     ],
   },
+
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from "next";
-export const dynamic = 'force-dynamic';
-import Header from "./components/header";
 import "./globals.css";
-import { Playfair_Display, Source_Sans_3 } from "next/font/google";
-import "tailwindcss";
+import Header from "./components/header";
 import Footer from "./components/footer";
 import WhatsAppButton from "./components/WhatsAppButton";
 import ThemeToggle from "./components/ThemeToggle";
-
+import { Playfair_Display, Source_Sans_3 } from "next/font/google";
 
 const playfair = Playfair_Display({ subsets: ["latin"], variable: "--font-playfair" });
 const sourceSans = Source_Sans_3({ subsets: ["latin"], variable: "--font-source-sans" });
@@ -15,9 +12,13 @@ const sourceSans = Source_Sans_3({ subsets: ["latin"], variable: "--font-source-
 export const metadata: Metadata = {
   title: "NewsFlash - Últimas Noticias",
   description: "Tras 620 días de agresión y genocidio en Palestina",
+  icons: {
+    icon: "/ico.ico",
+  },
   openGraph: {
     title: "NewsFlash - Últimas Noticias",
-    description: "Entregando las últimas noticias y actualizaciones de todo el mundo. Mantente informado con nuestra cobertura integral.",
+    description:
+      "Entregando las últimas noticias y actualizaciones de todo el mundo. Mantente informado con nuestra cobertura integral.",
     images: [
       {
         url: "https://69f7cd84c905.ngrok-free.app/ico.ico",
@@ -30,24 +31,19 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" >
-      <link rel="icon" href="/ico.ico" />
-      <body className={`${playfair.variable} ${sourceSans.variable}`}>
-        <div className="min-h-screen bg-gray-50 w-full"> 
-          <ThemeToggle/>
+    <html lang="es" className={`${playfair.variable} ${sourceSans.variable}`} suppressHydrationWarning>
+      <body suppressHydrationWarning>
+        <div className="min-h-screen bg-gray-50 w-full">
+          <ThemeToggle />
           <Header />
-          <div className="mx-auto px-4 py-4" style={{marginTop: "119px"}}>
-            {children}             
-          </div>  
+          <main className="mx-auto px-4 py-4" style={{ marginTop: "119px" }}>
+            {children}
+          </main>
+          <Footer />
+          <WhatsAppButton />
         </div>
-        <Footer />
-        <WhatsAppButton/>
       </body>
     </html>
   );
